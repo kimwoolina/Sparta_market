fc301273b4e6cbb58d3f0eee2ca5d5a9ea2be702
kimwoolina <rladnfls0500@gmail.com>
2024-08-20 23:10:47 +0900

    다중앱 생성 후 등록, 템플릿 폴더생성,index페이지 연결

--- spartamarket/spartamarket/urls.py
+++ spartamarket/spartamarket/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from products import views
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("index/", views.index, name="index"),
 ]
