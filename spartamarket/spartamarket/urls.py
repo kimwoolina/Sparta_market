71aed68f156ff594722f872fde6a9890b4f25807
kimwoolina <rladnfls0500@gmail.com>
2024-08-21 00:56:50 +0900

    add base.html css code

--- spartamarket/spartamarket/urls.py
+++ spartamarket/spartamarket/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from products import views
 
@@ -24,3 +26,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("index/", views.index, name="index"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
