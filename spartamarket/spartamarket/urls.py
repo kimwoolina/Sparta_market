738bd29a1e57c4130ecaca11081838e938c572b6
kimwoolina <rladnfls0500@gmail.com>
2024-08-23 05:51:50 +0900

    feat: Add redirect from / to /index/

--- spartamarket/spartamarket/urls.py
+++ spartamarket/spartamarket/urls.py
@@ -20,11 +20,13 @@ from django.urls.conf import include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.urls import path, re_path
+from django.views.generic.base import RedirectView
 from products import views
 
 
 urlpatterns = [
-
+    re_path(r'^$', RedirectView.as_view(url='/index/', permanent=True)),
     path('admin/', admin.site.urls),
     path("index/", views.index, name="index"),
     path("accounts/", include("accounts.urls")),
