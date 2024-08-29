ab971a42aebb21cae8efcbbcc2b9b1723ffac154
kimwoolina <rladnfls0500@gmail.com>
2024-08-29 16:11:28 +0900

    refact: hide secret key

--- spartamarket/spartamarket/settings.py
+++ spartamarket/spartamarket/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+import json
+import os
+from django.core.exceptions import ImproperlyConfigured
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +24,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wd51_$ypf=hww84wukifs_y#ce+tlybr38#5pf14yk#ib3wy43'
+secret_file = os.path.join(BASE_DIR, 'secrets.json')
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+def get_secret(setting, secrets=secrets):
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+SECRET_KEY = get_secret("SECRET_KEY")
+#SECRET_KEY = 'django-insecure-wd51_$ypf=hww84wukifs_y#ce+tlybr38#5pf14yk#ib3wy43'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
